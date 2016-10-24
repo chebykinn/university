9871ed79d263ca65669970cf65c2dae932abf23c
Dmitry <dimaberserker@gmail.com>
2016-10-24 19:57:43 +0300

    nitial commit for lab2

---
+++ circuitry/lab2/src/reader.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    19:38:30 10/24/2016 
+// Design Name: 
+// Module Name:    reader 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module reader(
+    input CLK,
+    input CS,
+    input SDO,
+    output [7:0] data
+    );
+	 
+	 reg[7:0] data;
+
+
+endmodule
