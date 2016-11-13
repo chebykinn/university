84c809fc9f0b04354f6c99e674121f7d65e56d63
Dmitry Aytuganov <dimaberserker@gmail.com>
2016-11-14 00:15:24 +0300

    db_part2: initial commit

---
+++ dbms/courseWorkPart2/src/main/java/com/part2/Main.java
@@ -0,0 +1,32 @@
+package com.part2;
+import org.jooq.impl.DSL;
+import org.jooq.util.maven.example.tables.Persons;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.LogManager;
+
+public class Main {
+    static {
+        LogManager.getLogManager().reset();
+    }
+
+    public static void main(String[] args) {
+        String user = "coursework";
+        String password = "123456789";
+        String url = "jdbc:postgresql://chebykinn.ru:1488/coursework";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+
+            DSL.using(connection)
+                    .select(Persons.PERSONS.FIRST_NAME).from(Persons.PERSONS)
+                    .forEach(
+                            (l) -> System.out.println(l.get(0))
+                    );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
