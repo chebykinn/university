f529a49f27ae69333dba52c1d0bd7722f4a71660
Dmitry Aytuganov <dimaberserker@gmail.com>
2016-11-29 14:05:18 +0300

    dbms_mongo: CRUD in process...

--- dbms/coursework/src/mongodb/crud.js
+++ dbms/coursework/src/mongodb/crud.js
@@ -10,18 +10,31 @@ var rl = readline.createInterface({
 mongoose.connect('mongodb://192.168.1.123/coursework');
 var db = mongoose.connection;
 
-rl.on('line', (input) => {
-	var splitted_input = input.split(/\s+/);
-	console.log(splitted_input);
-});
-
 var funcs = { 
-	'add': function() {
+	'add': function(splitted_input) {
+
+
 	},
-	'read': function() {
+	'read': function(splitted_input) {
+		console.log('read');
 	},
-	'update': function() {
+	'update': function(splitted_input) {
+		console.log('update');
 	},
-	'delete': function() {
+	'delete': function(splitted_input) {
+		console.log('delete');
+	},
+	'help': function(splitted_input) {
+		console.log('help');
 	}
 }
+
+rl.on('line', (input) => {
+	var splitted_input = input.split(/\s+/);
+	if(splitted_input[0] in funcs) {
+		funcs[splitted_input[0]](splitted_input);
+	} else {
+		console.log('Unknown command, see help');
+	}
+
+});
