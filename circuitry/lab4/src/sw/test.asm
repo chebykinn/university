41c9eb85ed7e5d5c250bb496445f6c68ab0172ba
chebykinn <ivan@chebykinn.ru>
2016-12-13 19:57:10 +0300

    circuitry: finished lab4

--- circuitry/lab4/src/sw/test.asm
+++ circuitry/lab4/src/sw/test.asm
@@ -7,14 +7,19 @@
 .text                  /* code goes to text section*/
 .ent entry
 entry:
-	sw $t0, 0x400
+	addi $t0, $t0, 0x1488
+	.loop:
+		jal .iter
+		nop
+		beq $t0, $t1, .eq
+		j .loop
+		.iter:
+			addi $t1, $t1, 1
+			jr $ra
+		.eq:
+			addi $t2, $t2, 1
+			sw $t2, 0x400
+			andi $t1, $t1, 0
+			j .loop
 
-	jal .check
-	addi $t0, $t0, 1
-	j entry
-
-	.check:
-		addi $t0, $t0, 1
-		jr $t31
-		j entry
 .end entry
