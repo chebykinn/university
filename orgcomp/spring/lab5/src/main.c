08253fa1a720ad34fa378c76afcc165b83f9c73f
Ivan Chebykin <ivan@chebykin.org>
2017-04-06 23:00:29 +0300

    orgcomp_lab5: almost done

--- orgcomp/spring/lab5/src/main.c
+++ orgcomp/spring/lab5/src/main.c
@@ -1,74 +1,59 @@
 #include <reg51.h>
+float numb;
+char digit;
+char xdata mas[8];
+char m = 1;
+char mm;
+int i = 0;
 
-char w,x,i,digit,mas[7],min;
+void get_char(void) interrupt 0 {
+	char x = P3;
+	switch (x) {
+		case 0x7b: digit = '1'; break;
+		case 0x79: digit = '2'; break;
+		case 0x78: digit = '3'; break;
+		case 0xbb: digit = '4'; break;
+		case 0xb9: digit = '5'; break;
+		case 0xb8: digit = '6'; break;
+		case 0xdb: digit = '7'; break;
+		case 0xd9: digit = '8'; break;
+		case 0xd8: digit = '9'; break;
+		case 0xeb: digit = '0'; break;
+		case 0xe9: digit = ','; break;
+		case 0xe8: digit = 'e'; break;
+		case 0xc8: digit = '-'; break;
+		default: digit = 0xff;
+	}
 
-float numb,m;
+	if ( digit == ',' ) {
+		m = 1;
+		mas[i++] = digit;
+		return;
+	}
 
-char code scancode[]={ 0xdd,0x7b,0xde,0x7d,0x7e,0xb7,0xe7 }; //-1,234 esc
+	if ( digit == '-' ) {
+		numb = -numb;
+		mas[i++] = digit;
+		return;
+	}
 
-Delay(int t) { while(t--); }
+	if ( digit == 'e' ) {
+		if( m ) numb = numb / m;
+		i = 0;
+		m = 0;
+	} else {
+		mas[i++] = digit;
+		numb = numb * 10 + (digit & 0xf);
 
-char what(void);
+		if ( m ) m *= 10;
+		
+	}
 
-scan( ) interrupt 2 {
-	Delay(100);
-	w=scancode[i];
-	what();
-	Delay(100);
 }
 
-main () {
-	x=0xFE;
-	numb=0;
-	i=0;
-	m=0;
-	IT1= EX1=EA=1;
-	while (digit!='e') Delay(100);
-	while(1);
-}
-
-char what(void) {
-	switch (w) {
-		case 0x77: digit='0'; break;
-		case 0x7b: digit='1'; break;
-		case 0x7d: digit='2'; break;
-		case 0x7e: digit='3'; break;
-		case 0xb7: digit='4'; break;
-		case 0xbb: digit='5'; break;
-		case 0xbd: digit='6'; break;
-		case 0xbe: digit='7'; break;
-		case 0xd7: digit='8'; break;
-		case 0xdb: digit='9'; break;
-		case 0xdd: digit='-'; break;
-		case 0xde: digit=','; break;
-		case 0xe7: digit='e'; break;
-		default: digit=0xff;
-	}
-
-	if(digit==',') {
-		m=1;
-		mas[i++]=digit;
-		goto exit;
-	}
-
-	if(digit=='-') {
-		min=1;
-		mas[i++]=digit;
-		goto exit;
-	}
-
-	if(digit=='e') {
-		if(m) numb=numb/m;
-		i=0;
-		m=0;
-		goto exit;
-	}
-	else {
-		mas[i++]=digit;
-		numb=numb*10 + (digit&0xf);
-		if(m) m*=10;
-	}
-
-	exit:
-		return digit;
+void main() {
+	EX0 = 1;
+	IT0 = 1;
+	EA = 1;
+	while (1);
 }
