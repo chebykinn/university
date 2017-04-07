65d87cb76dae8d16db42675c1b42c3fe10d304d2
Ivan Chebykin <ivan@chebykin.org>
2017-04-07 23:21:08 +0300

    distributed_lab1: fixes

--- distributed/lab1/src/main.c
+++ distributed/lab1/src/main.c
@@ -16,8 +16,9 @@
 
 static const char e_no_args[]  = "Error: not enough arguments\n";
 static const char e_proc_num[] = "Error: number of processes should be from 1 to 10\n";
-static const char e_log_write[] = "Error: failed to write event to log\n";
+static const char e_log_write[] = "Error: failed to write event read log\n";
 static const char e_log_multicast[] = "Error: failed to send multicast\n";
+static const char e_log_multicast_read[] = "Error: failed to read multicast\n";
 
 static int events_log_fd = -1;
 
@@ -37,14 +38,30 @@ int receive_all(IOHandle *handle){
 	for(local_id i = 1; i < handle->proc_num; i++){
 		int rc = receive(handle, i, &msg);
 		if( rc != 0 ){
-			write(STDERR_FILENO, e_log_multicast, sizeof e_log_multicast);
+			write(STDERR_FILENO, e_log_multicast_read, sizeof e_log_multicast);
 			return 1;
 		}
 	}
 	return 0;
 }
 
+void close_pipes(IOHandle *handle){
+	Channel *out = &handle->channel_table[handle->src_pid * handle->proc_num];
+	close(out[handle->src_pid].readfd);
+	close(out[handle->src_pid].writefd);
+
+	for(int i = 0; i < handle->proc_num; i++){
+		close(out[i].readfd);
+	}
+
+	for(int i = 0; i < handle->proc_num; i++){
+		close(handle->channel_table[i * handle->proc_num + handle->src_pid].writefd);
+	}
+}
+
+
 int parent(IOHandle *handle){
+	close_pipes(handle);
 	int rc = 0;
 	rc = receive_all(handle);
 	if( rc != 0 ) return 1;
@@ -59,10 +76,7 @@ int parent(IOHandle *handle){
 }
 
 int child(IOHandle *handle, int sys_pid, int ppid){
-	Channel loop = handle->channel_table[handle->src_pid * handle->proc_num
-														 + handle->src_pid];
-	close(loop.readfd);
-	close(loop.writefd);
+	close_pipes(handle);
 	char log_buff[MAX_PAYLOAD_LEN];
 	int rc = 0;
 	Message msg;
@@ -140,13 +154,14 @@ int main(int argc, char *const argv[]) {
 	if( channel_table == NULL ) return 1;
 
 	IOHandle handle;
+	memset(&handle, 0, sizeof handle);
 	handle.proc_num = proc_num;
 	handle.channel_table = channel_table;
 
-	int pipes_log_fd = open(pipes_log, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	int pipes_log_fd = open(pipes_log, O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0644);
 	if( pipes_log_fd < 0 ) return 1;
 
-	events_log_fd = open(events_log, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	events_log_fd = open(events_log, O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0644);
 	if( events_log_fd < 0 ) return 1;
 
 	char msg[64];
@@ -159,6 +174,7 @@ int main(int argc, char *const argv[]) {
 			if( rc < 0 ) return 1;
 		}
 	}
+
 
 	pid_t ppid = getpid(), current_sys_pid = 0;
 	int is_parent = 1;
@@ -176,9 +192,12 @@ int main(int argc, char *const argv[]) {
 		}
 	}
 
+	int rc = 0;
 	if( is_parent ){
-		return parent(&handle);
+		rc = parent(&handle);
 	}else{
-		return child(&handle, current_sys_pid, ppid);
+		rc = child(&handle, current_sys_pid, ppid);
 	}
+	free(channel_table);
+	return rc;
 }
