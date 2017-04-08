2b88f9a8c0749a6f4218e9de5df25c8ccb6758f1
Ivan Chebykin <ivan@chebykin.org>
2017-04-08 18:08:43 +0300

    sfw_test_lab2: Added CSV Writer

--- sfw_test/lab2/src/main/java/lab/Functions.java
+++ sfw_test/lab2/src/main/java/lab/Functions.java
@@ -4,5 +4,5 @@ package lab;
  * Created by daituganov on 08.04.17.
  */
 public enum Functions {
-    LN, LOG_2, LOG_10, COSINUS, COTANGENT, SECANT, TANGENT,
+    STUB, LN, LOG_2, LOG_10, COSINUS, COTANGENT, SECANT, TANGENT,
 }
