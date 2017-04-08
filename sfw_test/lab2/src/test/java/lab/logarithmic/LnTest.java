549963993d7d4a4ce9de28bf4e5546619fa0d163
Ivan Chebykin <ivan@chebykin.org>
2017-04-08 18:10:12 +0300

    sfw_test_lab2: wip tests

--- sfw_test/lab2/src/test/java/lab/logarithmic/LnTest.java
+++ sfw_test/lab2/src/test/java/lab/logarithmic/LnTest.java
@@ -2,14 +2,11 @@ package lab.logarithmic;
 
 import org.junit.Test;
 
+import static lab.AbstractFunction.DELTA;
 import static org.junit.Assert.*;
 
 /**
  * Created by ivan on 08.04.17.
  */
-public class LogarithmTest {
-    @Test
-    public void calc() throws Exception {
-    }
-
+public class LnTest {
 }
